33ad0f1a00d64406a334136be71d4acb757a9a15
lenovo <lenovo@LAPTOP-P8K9KTT5>
2020-02-28 15:10:59 +0800

    2

--- L1_2_calcPixelAverage/L1_2_calcPixelAverage.cpp
+++ L1_2_calcPixelAverage/L1_2_calcPixelAverage.cpp
@@ -2,10 +2,15 @@
 //
 
 #include "stdafx.h"
-
+#include <opencv.hpp>
+using namespace cv;
 
 int main()
 {
-    return 0;
-}
 
+	cv::Mat srcMat = imread("D:\\testImage\\1.jpg",0);
+	imshow("src", srcMat);
+	waitKey();
+
+	return 0;
+}
